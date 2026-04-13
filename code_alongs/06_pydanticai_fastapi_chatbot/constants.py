1dfdb9b2662c076d8240feab3932a0aeeb0d82c1
Omer Aytug <aytugomer00@gmail.com>
2026-04-13 11:15:39 +0200

    changed te mid model

--- code_alongs/06_pydanticai_fastapi_chatbot/constants.py
+++ code_alongs/06_pydanticai_fastapi_chatbot/constants.py
@@ -1,3 +1,3 @@
 MODEL_SMALL="openrouter:liquid/lfm-2.5-1.2b-instruct:free"
-MODEL_MEDIUM="openrouter:google/gemma-3n-e4b-it:free"
+MODEL_MEDIUM="openrouter:nvidia/nemotron-3-nano-30b-a3b:free"
 MODEL_LARGE="openrouter:meta-llama/llama-3.3-70b-instruct:free"
