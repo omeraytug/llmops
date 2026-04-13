d7cda2e065edc1a80fecef9f4ca1c6524feb6ff6
Omer Aytug <aytugomer00@gmail.com>
2026-04-13 11:44:22 +0200

    config for the chatrequest model: better understanding for the user with an example

--- code_alongs/06_pydanticai_fastapi_chatbot/data_models.py
+++ code_alongs/06_pydanticai_fastapi_chatbot/data_models.py
@@ -5,6 +5,12 @@ class ChatRequest(BaseModel):
     question: str = Field(description="users message or question to JokeBot")
     message_history: list[ModelMessage] = []
 
+    model_config = {
+        "json_schema_extra": {
+            "example": {"question": "Write a joke about Göteborg", "message_history": []}
+        }
+    }
+
 class ChatResponse(BaseModel):
     response: str = Field(description="Jokebots response with programming joke and emojis")
     message_history: list[ModelMessage]
